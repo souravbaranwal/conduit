bb1580a6fc3782254df592f2614785c2af96cdcb
sourav kumar <sourav.baranwal@gmail.com>
2019-07-29 23:39:00 +0530

    update articleUserDisplay with NavLink and bug fixes

--- src/components/ArticleUserDisplay.js
+++ src/components/ArticleUserDisplay.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import UserMyArticle from "./UserMyArticle";
 import UserFavArticle from "./UserFavArticle";
 import Loader from "./Loader";
@@ -9,8 +9,7 @@ class ArticleUserDisplay extends Component {
     super(props);
     this.state = {
       user: null,
-      active: "myArticle",
-      
+      active: "myArticle"
     };
   }
 
@@ -35,23 +34,21 @@ class ArticleUserDisplay extends Component {
           <>
             <div className=" userDisplay" style={{ marginTop: "50px" }}>
               <div className="has-text-centered">
-                
-                  <div className="level">
-                    <div className="level-item has-text-centered">
-                      <figure className="image is-128x128 is-round has-text-centered">
-                        <img
-                          className="is-rounded"
-                          src={
-                            user.image
-                              ? user.image
-                              : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
-                          }
-                          alt="userImage"
-                        />
-                      </figure>
-                    </div>
+                <div className="level">
+                  <div className="level-item has-text-centered">
+                    <figure className="image is-128x128 is-round has-text-centered">
+                      <img
+                        className="is-rounded"
+                        src={
+                          user.image
+                            ? user.image
+                            : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+                        }
+                        alt="userImage"
+                      />
+                    </figure>
                   </div>
-                
+                </div>
 
                 <h3>{user.username}</h3>
 
@@ -61,34 +58,37 @@ class ArticleUserDisplay extends Component {
               </div>
             </div>
             <div className="container ">
-              
-                <div className="tabs">
-                  <ul>
-                    <li>
-                      <Link
-                        onClick={() => this.setState({ active: "myArticle" })}
-                      >
-                        My Articles
-                      </Link>
-                    </li>
+              <div className="tabs">
+                <ul>
+                  <li>
+                    <NavLink
+                      activeClassName="is-active"
+                      
+                      onClick={() => this.setState({ active: "myArticle" })}
+                    >
+                      My Articles
+                    </NavLink>
+                  </li>
 
-                    <li>
-                      <Link onClick={() => this.setState({ active: "fav" })}>
-                        Favorite Articles
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              
+                  <li>
+                    <NavLink
+                      activeClassName="is-active"
+                      onClick={() => this.setState({ active: "fav" })}
+                    >
+                      Favorite Articles
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
             {this.state.active === "myArticle" ? (
               <UserMyArticle user={user.username} />
             ) : (
-              <UserFavArticle user={user.username}/>
+              <UserFavArticle user={user.username} />
             )}
           </>
         ) : (
-          <Loader/>
+          <Loader />
         )}
       </>
     );
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -30,80 +30,82 @@ class Articles extends Component {
         <p>{this.state.tag}</p>
         <div className="columns">
           <div className="column is-9 ">
-            <div className="feed-section">
-              <h4 className="is-size-4">Global Feed</h4>
-            </div>
-            <hr />
             {this.state.inputTag == null ? (
-              articles ? (
-                articles.map((article, index) => {
-                  return (
-                    <div className="box" key={index}>
-                      <article className="media">
-                        <div className="media-left">
-                          <figure className="image is-64x64">
-                            <img
-                              src={
-                                article.author.image
-                                  ? article.author.image
-                                  : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
-                              }
-                              alt="authorImage"
-                            />
-                          </figure>
-                        </div>
-                        <div className="media-content">
-                          <div className="content">
-                            <p className=" is-marginless">
-                              <Link
-                                to={{
-                                  pathname: "/articles/user",
-                                  state: {
-                                    username: article.author.username
-                                  }
-                                }}
-                              >
-                                <strong>{article.author.username}</strong>{" "}
-                              </Link>
-                              <small>created at</small>{" "}
-                              <small>{article.createdAt.substr(0, 10)}</small>
-                              <br />
-                            </p>
-                            <p className="is-size-4  is-marginless='true' ">
-                              <Link
-                                className="has-text-dark"
-                                to={{
-                                  pathname: "/article",
-                                  state: {
-                                    slug: article.slug,
-                                    username: article.author.username
-                                  }
-                                }}
-                              >
-                                {article.title}
-                              </Link>
-                            </p>
-                            <p className="is-size-6 is-marginless='true'">
-                              {article.description}
-                            </p>
+              <>
+                <div className="feed-section">
+                  <h4 className="is-size-4">Global Feed</h4>
+                  <hr />
+                </div>
+                {articles ? (
+                  articles.map((article, index) => {
+                    return (
+                      <div className="box" key={index}>
+                        <article className="media">
+                          <div className="media-left">
+                            <figure className="image is-64x64">
+                              <img
+                                src={
+                                  article.author.image
+                                    ? article.author.image
+                                    : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+                                }
+                                alt="authorImage"
+                              />
+                            </figure>
                           </div>
-                        </div>
-                        <div className="media-right">
-                          <a
-                            href="/#"
-                            className="button is-primary is-outlined"
-                          >
-                            <i className="fas fa-heart" />
-                            {article.favoritesCount}
-                          </a>
-                        </div>
-                      </article>
-                    </div>
-                  );
-                })
-              ) : (
-                <Loader />
-              )
+                          <div className="media-content">
+                            <div className="content">
+                              <p className=" is-marginless">
+                                <Link
+                                  to={{
+                                    pathname: "/articles/user",
+                                    state: {
+                                      username: article.author.username
+                                    }
+                                  }}
+                                >
+                                  <strong>{article.author.username}</strong>{" "}
+                                </Link>
+                                <small>created at</small>{" "}
+                                <small>{article.createdAt.substr(0, 10)}</small>
+                                <br />
+                              </p>
+                              <p className="is-size-4  is-marginless='true' ">
+                                <Link
+                                  className="has-text-dark"
+                                  to={{
+                                    pathname: "/article",
+                                    state: {
+                                      slug: article.slug,
+                                      username: article.author.username
+                                    }
+                                  }}
+                                >
+                                  {article.title}
+                                </Link>
+                              </p>
+                              <p className="is-size-6 is-marginless='true'">
+                                {article.description}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="media-right">
+                            <a
+                              href="/#"
+                              className="button is-primary is-outlined"
+                            >
+                              <i className="fas fa-heart" />
+                              {article.favoritesCount}
+                            </a>
+                          </div>
+                        </article>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <Loader />
+                )}
+              </>
             ) : (
               "hi"
             )}
